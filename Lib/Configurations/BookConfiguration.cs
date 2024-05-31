99921cf0c184f8ac08b546a82182be07ba96737d
xexexeee <144843286+xexexeee@users.noreply.github.com>
2024-05-31 18:13:12 +0300

    Добавил контекст и конфигурацию для книг

---
+++ Lib/Configurations/BookConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Lib.Models;
+namespace Lib.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder
+                .HasOne(b => b.author)
+                .WithMany(a => a.Books)
+                .HasForeignKey(b => b.AuthorId);
+
+            builder
+                .HasOne(b=>b.ReleaseYear)
+                .WithMany(ry => ry.Books)
+                .HasForeignKey(b=>b.YearId);
+
+            builder
+                .HasOne(b=>b.language)
+                .WithMany(l => l.Books)
+                .HasForeignKey(b=>b.LanguageId);
+
+            builder
+                .HasOne(b => b.publisher)
+                .WithMany(p => p.Books)
+                .HasForeignKey(b => b.PublisherId);
+
+            builder
+                .HasOne(b => b.country)
+                .WithMany(c => c.Books)
+                .HasForeignKey(b => b.CountryId);
+
+            builder
+                .HasOne(b => b.Genre)
+                .WithMany(g => g.Books)
+                .HasForeignKey(b=>b.GenreId);
+        }
+    }
+}
---
+++ Lib/Context/LibContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lib.Models;
+using Lib.Configurations;
+
+namespace Biblioteka.Context
+{
+    public class LibContext : DbContext
+    {
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    //base.OnModelCreating(modelBuilder);
+            
+        //}
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<ReleaseYear> Releaseyears { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public LibContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBiulder)
+        {
+            optionsBiulder.UseNpgsql("Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=8346");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+
+}
